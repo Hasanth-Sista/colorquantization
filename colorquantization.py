3aafcfe72a10ddc7e3203305fdca2f68e90f6196
Hasanth <hasanth1993@gmail.com>
2018-11-13 00:31:27 -0600

    version 2

--- colorquantization.py
+++ colorquantization.py
@@ -1,33 +1,18 @@
-from skimage import io
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 import numpy as np
+import argparse
+import cv2, imp, io, os
 
-image = io.imread('image1.jpg')
-rows = image.shape[0]
-cols = image.shape[1]
-image = image.reshape(image.shape[0] * image.shape[1], 3)
-kmeans = KMeans(n_clusters=5, max_iter=300)
-kmeans.fit(image)
+image = cv2.imread("image1.jpg")
+(h, w) = image.shape[:2]
+image = image.reshape((image.shape[0] * image.shape[1], 3))
 
-clusters = np.asarray(kmeans.cluster_centers_, dtype=np.uint8)
+clt = MiniBatchKMeans(n_clusters=5)
+labels = clt.fit_predict(image)
+quant = clt.cluster_centers_.astype("uint8")[labels]
+quant = quant.reshape((h, w, 3))
+image = image.reshape((h, w, 3))
 
-labels = np.asarray(kmeans.labels_, dtype=np.uint8)
-labels = labels.reshape(rows, cols)
-
-
-np.save('codebook_tiger.npy', clusters)
-io.imsave('compressed_tiger.jpg', labels)
-
-
-centers = np.load('codebook_tiger.npy')
-
-c_image = io.imread('compressed_tiger.jpg')
-print(c_image.shape[0])
-# image = np.zeros((c_image.shape[0], c_image.shape[1], 3))
-#
-# for i in range(c_image.shape[0]):
-#     for j in range(c_image.shape[1]):
-#         image[i, j, :] = centers[c_image[i, j], :]
-#
-# io.imsave('reconstructed_tiger.jpg', image)
-
+cv2.imshow("image", np.hstack([image, quant]))
+cv2.imwrite("image.jpg", image)
+cv2.waitKey(0)
