193e88d6c86445d0b1e0c6bb68a577ea7611288f
Hasanth <hasanth1993@gmail.com>
2018-11-13 18:41:38 -0600

    colorquantization changed

--- colorquantization.py
+++ colorquantization.py
@@ -2,37 +2,38 @@ from skimage import io
 from sklearn.cluster import KMeans
 import numpy as np
 
-image = io.imread('image1.jpg')
-io.imshow(image)
-io.show()
 
-rows = image.shape[0]
-cols = image.shape[1]
+for i in range(1, 4):
+    image = io.imread('image'+str(i)+'.jpg')
+    io.imshow(image)
+    io.show()
 
-image = image.reshape(image.shape[0] * image.shape[1], 3)
-kmeans = KMeans(n_clusters=128, n_init=10, max_iter=200)
-kmeans.fit(image)
+    rows = image.shape[0]
+    cols = image.shape[1]
 
-clusters = np.asarray(kmeans.cluster_centers_, dtype=np.uint8)
-labels = np.asarray(kmeans.labels_, dtype=np.uint8)
-labels = labels.reshape(rows, cols)
+    image = image.reshape(image.shape[0] * image.shape[1], 3)
+    kmeans = KMeans(n_clusters=5, n_init=10, max_iter=200)
+    kmeans.fit(image)
 
-np.save('codebook_tiger.npy', clusters)
-io.imsave('compressed_tiger.png', labels)
+    clusters = np.asarray(kmeans.cluster_centers_, dtype=np.uint8)
+    labels = np.asarray(kmeans.labels_, dtype=np.uint8)
+    labels = labels.reshape(rows, cols)
 
-from skimage import io
-import numpy as np
+    np.save('npyfiles/codebook_tiger'+str(i)+'.npy', clusters)
+    io.imsave('npyfiles/compressed_tiger'+str(i)+'.png', labels)
 
-centers = np.load('codebook_tiger.npy')
-c_image = io.imread('compressed_tiger.png')
+    centers = np.load('npyfiles/codebook_tiger'+str(i)+'.npy')
+    c_image = io.imread('npyfiles/compressed_tiger'+str(i)+'.png')
 
-image = np.zeros((c_image.shape[0], c_image.shape[1], 3), dtype=np.uint8)
-for i in range(c_image.shape[0]):
-    for j in range(c_image.shape[1]):
-        image[i, j, :] = centers[c_image[i, j], :]
-io.imsave('reconstructed_tiger.png', image)
-io.imshow(image)
-io.show()
+    image = np.zeros((c_image.shape[0], c_image.shape[1], 3), dtype=np.uint8)
+
+    for j in range(c_image.shape[0]):
+        for k in range(c_image.shape[1]):
+            image[j, k, :] = centers[c_image[j, k], :]
+
+    io.imsave('quantizedImages/compressed'+str(i)+'.png', image)
+    io.imshow(image)
+    io.show()
 
 
 # from sklearn.cluster import MiniBatchKMeans
