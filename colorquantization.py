f8eb9ec7b7aae6dbde12aa8b7451deb949ceeb0a
Hasanth <hasanth1993@gmail.com>
2018-11-17 22:09:32 -0600

    pca code

--- colorquantization.py
+++ colorquantization.py
@@ -12,18 +12,18 @@ for i in range(1, 4):
     cols = image.shape[1]
 
     image = image.reshape(image.shape[0] * image.shape[1], 3)
-    kmeans = KMeans(n_clusters=5, n_init=10, max_iter=200)
+    kmeans = KMeans(n_clusters=10, n_init=10, max_iter=200)
     kmeans.fit(image)
 
     clusters = np.asarray(kmeans.cluster_centers_, dtype=np.uint8)
     labels = np.asarray(kmeans.labels_, dtype=np.uint8)
     labels = labels.reshape(rows, cols)
 
-    np.save('npyfiles/codebook_tiger'+str(i)+'.npy', clusters)
-    io.imsave('npyfiles/compressed_tiger'+str(i)+'.png', labels)
+    np.save('npyfiles/quantized_image_code'+str(i)+'.npy', clusters)
+    io.imsave('npyfiles/quantized_image'+str(i)+'.png', labels)
 
-    centers = np.load('npyfiles/codebook_tiger'+str(i)+'.npy')
-    c_image = io.imread('npyfiles/compressed_tiger'+str(i)+'.png')
+    centers = np.load('npyfiles/quantized_image_code'+str(i)+'.npy')
+    c_image = io.imread('npyfiles/quantized_image'+str(i)+'.png')
 
     image = np.zeros((c_image.shape[0], c_image.shape[1], 3), dtype=np.uint8)
 
@@ -31,26 +31,6 @@ for i in range(1, 4):
         for k in range(c_image.shape[1]):
             image[j, k, :] = centers[c_image[j, k], :]
 
-    io.imsave('quantizedImages/compressed'+str(i)+'.png', image)
+    io.imsave('quantizedImages/quantized'+str(i)+'.png', image)
     io.imshow(image)
     io.show()
-
-
-# from sklearn.cluster import MiniBatchKMeans
-# import numpy as np
-# import argparse
-# import cv2, imp, io, os
-#
-# image = cv2.imread("image1.jpg")
-# (h, w) = image.shape[:2]
-# image = image.reshape((image.shape[0] * image.shape[1], 3))
-#
-# clt = MiniBatchKMeans(n_clusters=5)
-# labels = clt.fit_predict(image)
-# quant = clt.cluster_centers_.astype("uint8")[labels]
-# quant = quant.reshape((h, w, 3))
-# image = image.reshape((h, w, 3))
-#
-# cv2.imshow("image", np.hstack([image, quant]))
-# cv2.imwrite("image.jpg", image)
-# cv2.waitKey(0)
--- pca.py
+++ pca.py
@@ -1,26 +1,17 @@
 from sklearn.decomposition import PCA
-from pylab import *
-from skimage import data, io, color
+import cv2
 
-link = "image1.jpg"
-image = io.imread(link)
+# give absolute path for image input
+input = cv2.imread("image1.jpg")
 
-# subplot(2, 2, 1)
-# io.imshow(image)
-# xlabel('Original Image')
+output = "compressedImages/"
 
-for i in range(1, 2):
-    n_comp = 1 ** i
-    pca = PCA(n_components=n_comp)
-    image = image.reshape(image.shape[0] * image.shape[1], 3)
-    pca.fit(image)
-    coke_gray_pca = pca.fit_transform(image)
+for i in (5, 25, 50):
+    pca = PCA(n_components=i)
+    shape = input.shape
+    reshaped_image = input.reshape(shape[0], shape[1]*shape[2])
 
-    coke_gray_restored = pca.inverse_transform(coke_gray_pca)
-    # subplot(2, 2, i+1)
-    io.imshow(coke_gray_restored)
-    # xlabel('Restored image n_components = %s' %n_comp)
-    # print('Variance retained %s %%' %((1 - sum(pca.explained_variance_ratio_) / size(pca.explained_variance_ratio_)) * 100))
-    # print('Compression Ratio %s %%' %(float(size(coke_gray_pca)) / size(image) * 100))
-    # io.imsave(coke_gray_restored, 'image.jpg')
-    io.imsave('./colorquantization/', coke_gray_restored)
+    reduced_image = pca.fit_transform(reshaped_image)
+    reconstructed_image = pca.inverse_transform(reduced_image)
+
+    cv2.imwrite(output+"image"+str(i)+".jpg", reconstructed_image.reshape(shape))
